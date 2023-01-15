28161ddbd8376e926a008a024ebf9f420ed54b7d
sinhavishul244_windows <sinha.vishul244@gmail.com>
2023-01-15 14:10:38 +0530

    skeleton for menu page added

---
+++ codes/preloader.js
@@ -0,0 +1,9 @@
+var loader = document.getElementById("preloader");
+
+const loading_func = () => {
+    loader.style.display = "none";
+}
+
+const temp = setTimeout(loading_func, 500);
+
+window.addEventListener("load", temp);
