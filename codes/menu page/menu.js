554b2e6d8d6f3ae2e902f980845ac4fdc092b35b
sinhavishul244_windows <sinha.vishul244@gmail.com>
2023-01-28 17:10:26 +0530

    modified the menupage website : ready to merge

--- codes/menu page/menu.js
+++ codes/menu page/menu.js
@@ -1,104 +1,116 @@
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+document.addEventListener('DOMContentLoaded', starter);
 
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
 
-const mouse = {
-    x: innerWidth / 2,
-    y: innerHeight / 2
+function starter() {
+    let q = window.matchMedia("(max-width : 682px)");
+    if (q.matches) {
+        document.querySelector(".canvascontainer").innerHTML = " ";
+    }
+    else {
+        const canvas = document.querySelector('canvas')
+        const c = canvas.getContext('2d')
+
+        canvas.width = window.innerWidth
+        canvas.height = window.innerHeight
+
+        const mouse = {
+            x: innerWidth / 2,
+            y: innerHeight / 2
+        }
+
+        const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
+
+        // let button = document.querySelector(".anchor");
+
+
+        let mouseIn = false
+        // button.addEventListener('mouseover', () => {
+        //     mouseIn = true
+        // })
+
+        // addEventListener('mouseout', () => {
+        //     mouseIn = false
+        // })
+
+        addEventListener('resize', () => {
+            canvas.width = innerWidth
+            canvas.height = innerHeight
+
+            init()
+        })
+
+        // Objects
+        class Particle {
+            constructor(x, y, radius, color) {
+                this.x = x
+                this.y = y
+                this.radius = radius
+                this.color = color
+            }
+
+            draw() {
+                c.beginPath()
+                c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+                c.shadowColor = this.color
+                c.shadowBlur = 15
+                c.fillStyle = this.color
+                c.fill()
+                c.closePath()
+            }
+
+            update() {
+                this.draw()
+            }
+        }
+
+        // Implementation
+        let particles
+        function init() {
+            particles = []
+
+            for (let i = 0; i < 50; i++) {
+                const canvasWidth = canvas.width + 1000
+                const canvasHeight = canvas.height + 2000
+
+                const x = Math.random() * canvasWidth - canvasWidth / 2
+                const y = Math.random() * canvasHeight - canvasHeight / 2
+                const radius = 2 * Math.random()
+
+                const color = colors[Math.floor(Math.random() * colors.length)]
+                particles.push(new Particle(x, y, radius, color))
+            }
+        }
+
+        // Animation Loop
+        let radians = 0
+        let alpha = 1
+        function animate() {
+            requestAnimationFrame(animate)
+            c.fillStyle = `rgba(10, 10, 10, ${alpha})`
+            c.fillRect(0, 0, canvas.width, canvas.height)
+
+            c.save()
+            c.translate(canvas.width / 2, canvas.height / 2)
+            c.rotate(radians)
+            particles.forEach((particle) => {
+                particle.update()
+            })
+            c.restore()
+
+            radians += 0.003
+
+            if (mouseIn && alpha >= 0.03) {
+                alpha -= 0.01
+            } else if (!mouseIn && alpha < 1) {
+                alpha += 0.01
+            }
+        }
+
+        init()
+        animate()
+    }
 }
 
-const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
-
-// let button = document.querySelector(".anchor");
-
-
-let mouseIn = false
-// button.addEventListener('mouseover', () => {
-//     mouseIn = true
-// })
-
-// addEventListener('mouseout', () => {
-//     mouseIn = false
-// })
-
-addEventListener('resize', () => {
-    canvas.width = innerWidth
-    canvas.height = innerHeight
-
-    init()
-})
-
-// Objects
-class Particle {
-    constructor(x, y, radius, color) {
-        this.x = x
-        this.y = y
-        this.radius = radius
-        this.color = color
-    }
-
-    draw() {
-        c.beginPath()
-        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-        c.shadowColor = this.color
-        c.shadowBlur = 15
-        c.fillStyle = this.color
-        c.fill()
-        c.closePath()
-    }
-
-    update() {
-        this.draw()
-    }
-}
-
-// Implementation
-let particles
-function init() {
-    particles = []
-
-    for (let i = 0; i < 50; i++) {
-        const canvasWidth = canvas.width + 1000
-        const canvasHeight = canvas.height + 2000
-
-        const x = Math.random() * canvasWidth - canvasWidth / 2
-        const y = Math.random() * canvasHeight - canvasHeight / 2
-        const radius = 2 * Math.random()
-
-        const color = colors[Math.floor(Math.random() * colors.length)]
-        particles.push(new Particle(x, y, radius, color))
-    }
-}
-
-// Animation Loop
-let radians = 0
-let alpha = 1
-function animate() {
-    requestAnimationFrame(animate)
-    c.fillStyle = `rgba(10, 10, 10, ${alpha})`
-    c.fillRect(0, 0, canvas.width, canvas.height)
-
-    c.save()
-    c.translate(canvas.width / 2, canvas.height / 2)
-    c.rotate(radians)
-    particles.forEach((particle) => {
-        particle.update()
-    })
-    c.restore()
-
-    radians += 0.003
-
-    if (mouseIn && alpha >= 0.03) {
-        alpha -= 0.01
-    } else if (!mouseIn && alpha < 1) {
-        alpha += 0.01
-    }
-}
-
-init()
-animate()
 
 // script for swiper
 var swiper = new Swiper(".mySwiper", {
